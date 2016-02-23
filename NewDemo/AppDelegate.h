7bfcab05832a754d27453c338ae2ca549ec91f51
俊王 <junwang@junwangdeiMac.local>
2016-02-23 11:11:57 +0800

    Release 1.0.0

---
+++ NewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NewDemo
+//
+//  Created by 俊王 on 16/2/23.
+//  Copyright © 2016年 俊王. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NewDemo
+//
+//  Created by 俊王 on 16/2/23.
+//  Copyright © 2016年 俊王. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
